fdafa732509bb331d6cf304863297c2af864ada6
Zhekai Li <s2139833@bioinfmsc7.bio.ed.ac.uk>
2024-11-18 01:37:09 +0000

    sequence download

--- script.py
+++ script.py
@@ -2,6 +2,17 @@ import os
 import subprocess
 import argparse
 
+def download_sequences(protein_family, taxonomy, output_path):
+    file_name = protein_family+'_'+taxonomy+'_sequences.fasta'
+    fasta_file = os.path.join(output_path, file_name)
+    command = (
+        f"esearch -db protein -query '{protein_family}[PROTEIN] AND {taxonomy}[ORGANISM]' | "
+        f"efetch -format fasta > {fasta_file}"
+    )
+    print(f"esearch {protein_family}")
+    subprocess.run(command, shell=True, check=True)
+    print(f"Sequences saved to {fasta_file}")
+
 def parse_args():
     parser = argparse.ArgumentParser(description="OptionalPythonICA: Generic sequence analysis tool")
     parser.add_argument("--protein_family", required=True, help="Specify the protein family")
@@ -12,7 +23,10 @@ def parse_args():
 def main():
     args = parse_args()
     os.makedirs(args.output_path, exist_ok=True)
-    print(f"Working directory created at {args.output_path}")
+    print(f"Output directory: {args.output_path}")
+    download_sequences(args.protein_family, args.taxonomy, args.output_path)
+
+
 
 if __name__ == "__main__":
     main()
